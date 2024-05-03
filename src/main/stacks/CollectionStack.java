d36cda97e58f04857723b7caa6c12f66af9a60ca
Ferhat <parhat002@gmail.com>
2024-05-03 21:26:33 +0300

    CollectionStack

---
+++ src/main/stacks/CollectionStack.java
@@ -0,0 +1,21 @@
+package main.stacks;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class CollectionStack {
+
+    public static void main(String[] args) {
+        Deque<Integer> cStack = new LinkedList<>();
+        cStack.push(1);
+        cStack.push(2);
+        cStack.push(3);
+        System.out.println(cStack);
+        System.out.println(cStack.peek());
+        System.out.println(cStack.pop());
+        System.out.println("Peek is: " + cStack.peek());
+
+        Stack<Integer> s = new Stack<>();
+    }
+}
