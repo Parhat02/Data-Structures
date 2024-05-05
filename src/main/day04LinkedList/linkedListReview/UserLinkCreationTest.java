643f9a0d298b9ad90b2a7aea4f67c37f1b11f352
Ferhat <parhat002@gmail.com>
2024-05-05 13:57:09 +0300

    LinkedListReview

--- src/main/day04LinkedList/linkedListReview/UserLinkCreationTest.java
+++ src/main/day04LinkedList/linkedListReview/UserLinkCreationTest.java
@@ -4,13 +4,12 @@ public class UserLinkCreationTest {
 
     public static void main(String[] args) {
 
-
-//        UserLinkedList list=new UserLinkedList();
-//        list.printNames();
-//        list.insertLast(new User("Jason", "AAA"));
-//        list.insertLast(new User("Jack","Reacher"));
-//        list.insertLast(new User("Carol","Reacher3"));
-//        list.printNames();
-//        list.get(2);
+        UserLinkedList list=new UserLinkedList();
+        list.printNames();
+        list.insertLast(new User("Jason", "AAA"));
+        list.insertLast(new User("Jack","Reacher"));
+        list.insertLast(new User("Carol","Reacher3"));
+        list.printNames();
+        //list.get(2);
     }
 }
