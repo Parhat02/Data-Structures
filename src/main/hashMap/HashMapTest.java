21641212b095c344edba44e92f382c2782ea8c7e
Ferhat <parhat002@gmail.com>
2024-05-26 20:18:46 +0300

    HashMap

---
+++ src/main/hashMap/HashMapTest.java
@@ -0,0 +1,23 @@
+package main.hashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        MyHashMap myHashMap=new MyHashMap();
+        myHashMap.put(1, 1);
+        myHashMap.put(2, 2);
+        myHashMap.put(5, 5);
+        myHashMap.put(100, 100);
+        System.out.println(myHashMap.get(100));
+        System.out.println(myHashMap.get(2));
+        myHashMap.put(2004, 2004);
+        System.out.println(myHashMap.get(2004));
+        myHashMap.remove(2);
+        System.out.println(myHashMap.get(2));
+        myHashMap.remove(100);
+        myHashMap.put(100, 100);
+        System.out.println(myHashMap.get(100));
+        myHashMap.put(1, 10);
+        System.out.println(myHashMap.get(1));
+    }
+
+}
---
+++ src/main/hashMap/KeyValue.java
@@ -0,0 +1,11 @@
+package main.hashMap;
+
+public class KeyValue <U, V>{
+    U key;
+    V value;
+
+    public KeyValue(U key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
