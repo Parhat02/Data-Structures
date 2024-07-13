f45fd30b8bffc1bf731a78ac89815dcbfca2f696
Ferhat <parhat002@gmail.com>
2024-07-13 17:18:47 +0300

    BubbleSort

--- src/main/sorting/TestSorting.java
+++ src/main/sorting/TestSorting.java
@@ -7,6 +7,6 @@ public class TestSorting {
         int[] numbers= {7,2,4,6,4,1,9,-1};
         System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
 
-        //System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
     }
 }
