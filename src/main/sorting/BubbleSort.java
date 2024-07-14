de7413356545ccf1a6a8d90acfa6f1102577d5ad
Ferhat <parhat002@gmail.com>
2024-07-14 21:31:40 +0300

    Review

--- src/main/sorting/BubbleSort.java
+++ src/main/sorting/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
         for (int i = 0; i < array.length; i++) {
             boolean swap=false;
             for (int j = 0; j < array.length-1; j++) {
-                if (array[j]>array[j]){
+                if (array[j]>array[j+1]){
                     swap(array, j, j+1);
                     swap=true;
                 }
